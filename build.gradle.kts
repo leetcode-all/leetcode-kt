3a45a652fdadd969302ca02b91d4bf6c583837e1
Aleksandr Lisianoi <aleksandr.lisianoi@rise-world.com>
2019-10-10 14:33:39 +0200

    Add gradle 'attempt' task

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,3 +22,45 @@ subprojects {
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     }
 }
+
+open class LeetcodeAttempt : DefaultTask() {
+    @set:Option(option = "number", description = "The number of the leetcode problem, XXXX")
+    @get:Input
+    var number: String = ""
+
+    @TaskAction
+    fun foo() {
+        val numberPattern = "[0-9]{4}".toRegex()
+
+        if (!numberPattern.matches(number)) {
+            println("Please provide a valid problem number, was $number")
+
+            return
+        }
+
+        if (!File("${project.projectDir}/problem-$number").mkdir()) {
+            println("Directory problem-$number already exists")
+
+            return
+        }
+
+        for (candidate in File("${project.projectDir}/problem-XXXX").listFiles() ?: emptyList()) {
+            if (candidate.isDirectory && candidate.name.matches("build".toRegex())) {
+                continue // will skip build directory if it exists
+            }
+
+            candidate.copyRecursively(File("${project.projectDir}/problem-$number/${candidate.name}"))
+        }
+
+        File("${project.projectDir}/problem-$number").walk()
+            .forEach {
+                if (it.isDirectory) {
+                    return@forEach
+                }
+
+                it.writeText(it.readText().replace("problemXXXX", "problem$number"))
+            }
+    }
+}
+
+tasks.register<LeetcodeAttempt>("attempt")
